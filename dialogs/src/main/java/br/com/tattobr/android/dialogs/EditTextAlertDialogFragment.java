b0948f881c73df146309416a84016d393deaa7c3
Rodrigo Botelho <tato.rodrigo@gmail.com>
2016-01-28 09:15:29 -0200

    Added inputType and params for the EditTextDialog

--- dialogs/src/main/java/br/com/tattobr/android/dialogs/EditTextAlertDialogFragment.java
+++ dialogs/src/main/java/br/com/tattobr/android/dialogs/EditTextAlertDialogFragment.java
@@ -13,14 +13,20 @@ public class EditTextAlertDialogFragment extends DialogFragment {
     private static final String TITLE = "title";
     private static final String MESSAGE = "message";
     private static final String TEXT = "text";
+    private static final String INPUT_TYPE = "input_type";
+    private static final String PARAMS = "params";
 
     public interface EditTextAlertDialogFragmentListener {
-        void onEditTextDialogPositiveButton(int tag, String text);
+        void onEditTextDialogPositiveButton(int tag, String text, Bundle params);
 
-        void onEditTextDialogNegativeButton(int tag);
+        void onEditTextDialogNegativeButton(int tag, Bundle params);
     }
 
     public static EditTextAlertDialogFragment newInstance(int tag, int title, int message, String text) {
+        return newInstance(tag, title, message, text, -1, null);
+    }
+
+    public static EditTextAlertDialogFragment newInstance(int tag, int title, int message, String text, int inputType, Bundle params) {
         EditTextAlertDialogFragment frag = new EditTextAlertDialogFragment();
 
         Bundle args = new Bundle();
@@ -28,6 +34,8 @@ public class EditTextAlertDialogFragment extends DialogFragment {
         args.putInt(TITLE, title);
         args.putInt(MESSAGE, message);
         args.putString(TEXT, text);
+        args.putInt(INPUT_TYPE, inputType);
+        args.putBundle(PARAMS, params);
         frag.setArguments(args);
 
         return frag;
@@ -40,9 +48,15 @@ public class EditTextAlertDialogFragment extends DialogFragment {
         final View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_edit_text, null);
         final EditText editText = (EditText) view.findViewById(android.R.id.text1);
         final TextInputLayout textInputLayout = (TextInputLayout) view.findViewById(R.id.hint);
+        final int tag = args.getInt(TAG);
+        final Bundle params = args.getBundle(PARAMS);
 
         textInputLayout.setHint(getString(args.getInt(MESSAGE)));
         editText.setText(args.getString(TEXT));
+        final int inputType = args.getInt(INPUT_TYPE);
+        if (inputType >= 0) {
+            editText.setRawInputType(inputType);
+        }
 
         int title = args.getInt(TITLE);
         if (title != 0) {
@@ -53,7 +67,7 @@ public class EditTextAlertDialogFragment extends DialogFragment {
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 if (getActivity() instanceof EditTextAlertDialogFragmentListener) {
-                    ((EditTextAlertDialogFragmentListener) getActivity()).onEditTextDialogPositiveButton(args.getInt(TAG), editText.getText().toString());
+                    ((EditTextAlertDialogFragmentListener) getActivity()).onEditTextDialogPositiveButton(tag, editText.getText().toString(), params);
                 }
             }
         });
@@ -61,7 +75,7 @@ public class EditTextAlertDialogFragment extends DialogFragment {
         builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 if (getActivity() instanceof EditTextAlertDialogFragmentListener) {
-                    ((EditTextAlertDialogFragmentListener) getActivity()).onEditTextDialogNegativeButton(args.getInt(TAG));
+                    ((EditTextAlertDialogFragmentListener) getActivity()).onEditTextDialogNegativeButton(tag, params);
                 }
             }
         });
